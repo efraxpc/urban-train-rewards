e987cbacdcc281d44928e1cda066e66c2c270faa
Efrain <efraxpc@gmail.com>
2019-09-25 22:50:14 -0500

    change

--- database/seeds/WellcomeEmailInfoSeeder.php
+++ database/seeds/WellcomeEmailInfoSeeder.php
@@ -12,7 +12,7 @@ class WellcomeEmailInfoSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('wellcome_email_info')->insert([
+        DB::table('wellcome_email_info')->insert([ 
             'tittle' => 'wellcome to Rewards System',
             'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac turpis tincidunt, convallis lorem consectetur, varius ipsum. Quisque mauris sem, tempus sit amet massa ac, efficitur tristique turpis. Aliquam maximus aliquam odio non facilisis. Mauris pellentesque blandit posuere.
             Donec rutrum, risus vitae aliquet fringilla, velit ligula placerat ligula, eget dapibus velit lacus et nibh. Nulla non lorem eget erat iaculis egestas a sed felis. Ut nec semper nisi. Cras in hendrerit lorem. Integer iaculis ex diam.',
