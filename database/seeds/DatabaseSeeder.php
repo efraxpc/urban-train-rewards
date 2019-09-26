54a9e717001be8833a1b204e4f5228fbc95617a4
Efrain <efraxpc@gmail.com>
2019-09-25 22:56:36 -0500

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(OffersTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(MailchipInfoSeeder::class);
-        $this->call(WellcomeEmailSeeder::class);
         $this->call(ContactInfoSeeder::class);
     }
 }
