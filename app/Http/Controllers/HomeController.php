58e8626a93db0d6e4992fba2a21c613d027e51ed
Efrain <efraxpc@gmail.com>
2019-09-29 00:14:46 -0500

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,22 +23,20 @@ class HomeController extends Controller
     {
         $user = auth()->user();
 
-        if(!is_null($user)){
-            $prize_categories_query = DB::table('rewards')
-                ->join('offer', 'rewards.offer_id', '=', 'offer.id')
-                ->join('prize_category', 'prize_category.id', '=', 'offer.prize_category_id')
-                ->join('rewards_user', 'rewards_user.user_id', '=', 'rewards.id')
-                ->join('users', 'rewards_user.user_id', '=', 'users.id')
-                ->where('users.id','=',$user->id)
-                ->select('prize_category.*')
-                ->get();
-            $collection = collect($prize_categories_query);
-            $prize_categories = $collection->unique()->values()->all();
-        }else {
-            $prize_categories = PrizeCategory::all();
+        $prize_categories = PrizeCategory::all();
 
-        }
-
+        /*
+           $prize_categories_query = DB::table('rewards')
+               ->join('offer', 'rewards.offer_id', '=', 'offer.id')
+               ->join('prize_category', 'prize_category.id', '=', 'offer.prize_category_id')
+               ->join('rewards_user', 'rewards_user.user_id', '=', 'rewards.id')
+               ->join('users', 'rewards_user.user_id', '=', 'users.id')
+               ->where('users.id','=',$user->id)
+               ->select('prize_category.*')
+               ->get();
+           $collection = collect($prize_categories_query);
+           $prize_categories = $collection->unique()->values()->all();
+           */
 
         $contact_info = ContactInfo::find(1);
         return view('pages.frontend.home.index',compact('prize_categories','contact_info'));
